e08a0dbb2c6f4dca677297441a7efcbd9751754a
adonisja <nicoyhunt@gmail.com>
2026-06-05 07:22:13 -0400

    fix: remove double require_configured and unused ActivityIndicator import in credentials feature

--- mobile/src/screens/Trainee/MyCredentialsScreen.tsx
+++ mobile/src/screens/Trainee/MyCredentialsScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
 import {
-  View, Text, TouchableOpacity, StyleSheet, ActivityIndicator,
+  View, Text, TouchableOpacity, StyleSheet,
 } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import ScreenShell from '@components/ui/ScreenShell';
